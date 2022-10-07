ef286f6e0f9dbbb212b74c1caf7991c083e90c9b
Archie Sage <archie.sage@kcl.ac.uk>
2022-10-07 14:08:50 +0100

    Fix returned primes list length within primes function

--- primes.py
+++ primes.py
@@ -8,14 +8,20 @@ def primes(number_of_primes):
     
     if number_of_primes <= 0:
             raise ValueError()
+    elif number_of_primes == 1:
+        return [2]
+    elif number_of_primes == 2:
+        return [2, 3]
+    elif number_of_primes == 3:
+        return [2,3, 5]
+    else:
+        list = [2, 3, 5, 7]
+        count = 4
 
-    list = [2, 3, 5, 7]
-    count = 4
+        while len(list) < number_of_primes:
+            
+            if count % 2 != 0 and count % 3 != 0 and count % 5 != 0 and count % 7 != 0:
+                list.append(count)
+            count += 1
 
-    while len(list) < number_of_primes:
-        
-        if count % 2 != 0 and count % 3 != 0 and count % 5 != 0 and count % 7 != 0:
-            list.append(count)
-        count += 1
-
-    return list
+        return list
