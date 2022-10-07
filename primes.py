6b0257b484e5f399d105e22cc0cf73bace474442
Archie Sage <archie.sage@kcl.ac.uk>
2022-10-07 14:02:50 +0100

    Add functionality to calculate prime number to upper bound

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from calendar import c
+
+
 def primes(number_of_primes):
-    list = []
+    
+    if number_of_primes == 0:
+            raise ValueError()
+
+    list = [2, 3, 5, 7]
+    count = 4
+
+    while len(list) < number_of_primes:
+        
+        if count % 2 != 0 and count % 3 != 0 and count % 5 != 0 and count % 7 != 0:
+            list.append(count)
+        count += 1
+
     return list
